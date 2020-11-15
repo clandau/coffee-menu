66249a2d6274e93dbb0077523a7b1037d40b4536
courtney l <clandau@learnics.com>
2020-11-15 10:39:22 -0500

    display coffees and ingredients

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,11 +1,45 @@
 <template>
   <div>
-    <v-container></v-container>
+    <v-container>
+      <v-layout row wrap>
+        <v-flex
+          xs12
+          md6
+          lg4
+          v-for="(coffee, index) in coffees"
+          :key="index"
+        >
+        <v-card>
+          <v-card-title>
+            {{coffee.name}}
+          </v-card-title>
+          <v-card-content>
+            <v-list>
+              <v-list-item v-for="(ingredient, i) in coffee.ingredients" :key="i">
+                {{ ingredient }}
+              </v-list-item>
+            </v-list>
+          </v-card-content>
+        </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      coffees: [
+        {
+          name: "Mocha",
+          ingredients: ["chocolate", "espresso", "milk"]
+        }
+      ]
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped></style>
